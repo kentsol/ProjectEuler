2195a33a8845afa0eebfc79f918645ff26a4d46d
Kent Solvang <kent.solvang@gmail.com>
2020-02-23 17:10:47 +0100

    Add problem 3

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,9 +7,15 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Solution to Problem 1 is: { MultiplesOfThreeAndFive.GetSumOfMultiples()}" );
-            Console.WriteLine($"Solution to Problem 2 is: { EvenFibonacciNumbers.GetAnswer()}");
+            //Console.WriteLine($"Solution to Problem 1 is: { MultiplesOfThreeAndFive.GetSumOfMultiples()}" );
+            //Console.WriteLine($"Solution to Problem 2 is: { EvenFibonacciNumbers.GetAnswer()}");
+            //Console.WriteLine(LargestPrimeFactor.Solution());
 
+            DateTime startTime = DateTime.Now;
+            
+            //Insert problem here:
+            
+            Console.WriteLine($"The operation took {(DateTime.Now -startTime).TotalMilliseconds} ms.");
 
             Console.ReadLine();
         }
---
+++ ProblemLibrary/Problem3/LargestPrimeFactor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemLibrary
+{
+    public static class LargestPrimeFactor
+    {
+        private static long numberToSolve = 600851475143;
+        public static long Solution()
+        {
+            long testNumber = numberToSolve;
+            long highestPrime = 0;
+            long counter = 1;
+            long remains = testNumber;
+
+            for (long i = 0; i < testNumber; i++)
+            {
+               
+                if (counter > remains) return highestPrime;
+                if (remains % counter == 0)
+                {
+                    remains /= counter;
+                    highestPrime = counter;
+                    counter = 2;
+                }
+                else
+                {
+                    counter++;
+                }
+                
+            }
+            return highestPrime;
+        }
+    }
+}
